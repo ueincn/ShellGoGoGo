5da1c8101c0ebb957f70f6f0a968ed1b0e337d78
ueincn <fufouaka@163.com>
2023-07-25 19:40:13 +0800

    VM系统环境检测脚本

---
+++ Scripts/KVM/VM环境检测.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Program
+#   系统 KVM 环境检测
+#History
+#2023   Ueincn  Release
+
+function EnvCheck(){
+    #系统虚拟化环境检测
+    echo "系统环境检测开始："
+    egrep "(svm|vmx)" /proc/cpuinfo >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "  系统虚拟化支持检测 ... PASS"
+    else
+        echo "  系统虚拟化支持检测 ... FAIL"
+    fi
+
+    lsmod | grep kvm >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "  KVM 模块检测 ... PASS"
+    else
+        echo "  KVM 模块检测 ... FAIL"
+    fi
+    echo ""
+    echo "系统环境检测结束！"
+}
+
+EnvCheck
