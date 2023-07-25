66a696f36b19baceb3e52f9857bfa3763a64aa47
ueincn <fufouaka@163.com>
2023-07-25 19:43:10 +0800

    更新

--- Scripts/KVM/VM环境检测.sh
+++ Scripts/KVM/VM环境检测.sh
@@ -8,18 +8,19 @@
 function EnvCheck(){
     #系统虚拟化环境检测
     echo "系统环境检测开始："
+    echo ""
     egrep "(svm|vmx)" /proc/cpuinfo >/dev/null 2>&1
     if [ $? -eq 0 ]; then
-        echo "  系统虚拟化支持检测 ... PASS"
+        echo "    系统虚拟化支持检测 ... PASS"
     else
-        echo "  系统虚拟化支持检测 ... FAIL"
+        echo "    系统虚拟化支持检测 ... FAIL（请开启系统虚拟化支持！）"
     fi
 
     lsmod | grep kvm >/dev/null 2>&1
     if [ $? -eq 0 ]; then
-        echo "  KVM 模块检测 ... PASS"
+        echo "    KVM 模块检测 ... PASS"
     else
-        echo "  KVM 模块检测 ... FAIL"
+        echo "    KVM 模块检测 ... FAIL（请开启KVM模块！）"
     fi
     echo ""
     echo "系统环境检测结束！"
