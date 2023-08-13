59c7656233bf6404bb492e4944cbff6a366296c2
ueincn <fufouaka@163.com>
2023-08-13 18:24:41 +0800

    update

--- ShellScripts/程序安装与卸载/PackageInstall.sh
+++ ShellScripts/程序安装与卸载/PackageInstall.sh
@@ -1,50 +1,49 @@
 #!/bin/bash
 
 #Program
-#   deb软件包安装
+#   软件包安装通用脚本
 #History
 #2023   Ueincn  Release
 #Using
-#./PackageInstall PackageName
+#把Install函数中的"XXX"换成要安装的程序名，然后再根据使用的发行版安装包工具把47行更改下即可使用。
 
-PKG=$1
-
-function SpinLine(){
-    PID=$!
-    echo -ne " ... "
-    while kill -0 $PID 2>/dev/null
-    do
-        spin[0]="-"
-        spin[1]="\\"
-        spin[2]="|"
-        spin[3]="/"
-        for i in "${spin[@]}"
-        do
-            echo -ne "$i"
-            sleep 0.1
-            echo -ne "\b"
-        done
-    done
-    if [ $? -eq "0" ]; then
-        echo "PASS"
-    fi
+function ShowSpin(){
+      $* &
+      PID=$!
+      local SPINLINE=('-' '/' '|' '\')
+      sleep 0.05
+      echo -n " ... "
+      while kill -0 $PID 2>/dev/null
+      do
+            for SPIN in "${SPINLINE[@]}"
+            do
+                  echo -ne "$SPIN"
+                  sleep 0.1
+                  echo -ne "\b"
+            done
+      done
 }
+function StatusCode(){
 
-function PackageInstall(){
-    echo -ne "$PKG 安装中"
-    apt-get install $PKG -y >/dev/null 2>&1 &
-    SpinLine
+      local PASS=$(tput setaf 2)
+      local FAIL=$(tput setaf 1)
+      local CLEAR=$(tput sgr0)
+      if [ $? -eq 0 ]; then
+            echo -e "\b${PASS}PASS${CLEAR}"
+      else
+            echo -e "\b${FAIL}FAIL${CLEAR}"
+      fi
 }
-
-function PackageCheck(){
-    dpkg -l | grep $PKG >/dev/null 2>&1
-    if [ $? == "0" ]; then
-        echo "$PKG 已安装！"
-        exit 0
-    else
-        echo "$PKG 未安装，执行安装程序！"
-    fi
+function EchoTitle(){
+      echo "$*"
 }
-
-PackageCheck
-PackageInstall
+function EchoSubTitle(){
+      echo -n "    $*"
+}
+function Install(){
+      EchoTitle "程序开始安装:"
+      EchoSubTitle "Install XXX"
+      apt-get install XXX >/dev/null 2>&1
+      StatusCode
+}
+ShowSpin Install
